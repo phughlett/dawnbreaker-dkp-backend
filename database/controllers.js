232feddce91dedd014de72f1ca2b4a8fd517238e
Patrick Hughlett <brian.hughlett@spaceforce.mil>
2022-06-24 17:57:35 -0600

    basic db controllers for gets added

---
+++ database/controllers.js
@@ -0,0 +1,22 @@
+const knex = require('./dbConnection');
+
+module.exports = {
+  //Raid Team
+
+  getRaidTeams: () => {
+    return knex('raid_teams').select();
+  }
+
+  //Characters
+
+  getCharacters: () => {
+    return knex('characters').select();
+  }
+
+  //Ledger
+
+  getLedger: () => {
+    return knex('ledger').select();
+  }
+
+}
